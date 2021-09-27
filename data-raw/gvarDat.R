581d7e3fa535ee013732ea07fde2f359454aa175
Trevor Swanson <trevorswanson222@gmail.com>
2021-09-27 18:20:18 -0500

    updated mlgvarDat

--- data-raw/gvarDat.R
+++ data-raw/gvarDat.R
@@ -3,7 +3,7 @@
 library(modnets)
 
 set.seed(666)
-gvarDat <- simNet(N = 5000, p = 5, m = TRUE, m2 = .3, m1 = .5,
+gvarDat <- simNet(N = 5000, p = 5, m = TRUE, m2 = .15, m1 = .5,
                   modType = 'partial', lags = 1)
 
 atts <- gvarDat[setdiff(names(gvarDat), 'data')]
